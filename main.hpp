952636c25a919a987fff3de9d84fcc81106a0cb5
Raghu-9544 <100403037+Raghu-9544@users.noreply.github.com>
2022-06-13 21:59:32 +0530

    Update main.hpp

--- main.hpp
+++ main.hpp
@@ -1,1 +1,23 @@
+#include<iostream>
 
+class ColorPair {
+        private:
+            MajorColor majorColor;
+            MinorColor minorColor;
+        public:
+            ColorPair(MajorColor major, MinorColor minor):
+                majorColor(major), minorColor(minor)
+            {}
+            MajorColor getMajor() {
+                return majorColor;
+            }
+            MinorColor getMinor() {
+                return minorColor;
+            }
+            std::string ToString() {
+                std::string colorPairStr = MajorColorNames[majorColor];
+                colorPairStr += " ";
+                colorPairStr += MinorColorNames[minorColor];
+                return colorPairStr;
+            }
+    };
