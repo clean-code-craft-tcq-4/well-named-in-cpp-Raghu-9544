e35a2ccdb349e2d9af4830d00827dd94778e265d
Raghu-9544 <100403037+Raghu-9544@users.noreply.github.com>
2022-06-13 22:12:46 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -32,7 +32,7 @@ namespace TelCoColorCoder
     }
 }
 
-int main() {
+int main()  {
     testNumberToPair(4, TelCoColorCoder::WHITE, TelCoColorCoder::BROWN);
     testNumberToPair(5, TelCoColorCoder::WHITE, TelCoColorCoder::SLATE);
 
