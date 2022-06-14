eeecbbb76ba6c1c78c9029b0c237a856a52d8456
Raghu-9544 <100403037+Raghu-9544@users.noreply.github.com>
2022-06-14 19:38:12 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -18,7 +18,7 @@ namespace TelCoColorCoder
     int numberOfMinorColors =
         sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
 
-}
+    
     ColorPair GetColorFromPairNumber(int pairNumber) {
         int zeroBasedPairNumber = pairNumber - 1;
         MajorColor majorColor = 
@@ -30,6 +30,7 @@ namespace TelCoColorCoder
     int GetPairNumberFromColor(MajorColor major, MinorColor minor) {
         return major * numberOfMinorColors + minor + 1;
     }    
+}
 
 int main()  {
     testNumberToPair(4, TelCoColorCoder::WHITE, TelCoColorCoder::BROWN);
