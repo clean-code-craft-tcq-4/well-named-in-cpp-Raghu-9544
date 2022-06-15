eb810545a6d48045a01857ebb4fbb8235b6745f8
Raghu-9544 <100403037+Raghu-9544@users.noreply.github.com>
2022-06-15 19:05:04 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 #include "main.h"
 
 
-    void testNumberToPair(int pairNumber,
+void testNumberToPair(int pairNumber,
     TelCoColorCoder::MajorColor expectedMajor,
     TelCoColorCoder::MinorColor expectedMinor)
 {
@@ -13,6 +13,7 @@
     std::cout << "Got pair " << colorPair.ToString() << std::endl;
     assert(colorPair.getMajor() == expectedMajor);
     assert(colorPair.getMinor() == expectedMinor);
+    std::cout << " For Color Code: " << colorPair.ToString() << ", Pair No is: " << pairNumber << std::endl;
 }
 
 void testPairToNumber(
@@ -23,7 +24,12 @@ void testPairToNumber(
     int pairNumber = TelCoColorCoder::GetPairNumberFromColor(major, minor);
     std::cout << "Got pair number " << pairNumber << std::endl;
     assert(pairNumber == expectedPairNumber);
+    TelCoColorCoder::ColorPair colorPair =
+        TelCoColorCoder::GetColorFromPairNumber(pairNumber);
+    std::cout << " For Pair No: " << pairNumber << ", Color Code is: " << colorPair.ToString() << std::endl;
 }
+
+
 int main()  {    
     testNumberToPair(4, TelCoColorCoder::WHITE, TelCoColorCoder::BROWN);
     testNumberToPair(5, TelCoColorCoder::WHITE, TelCoColorCoder::SLATE);
