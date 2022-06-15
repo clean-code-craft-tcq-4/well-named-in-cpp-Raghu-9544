b2780959c79a58a290d11e18851c7f5f0a36f294
Raghu-9544 <100403037+Raghu-9544@users.noreply.github.com>
2022-06-15 17:40:33 +0530

    Update colourcoder.h

--- colourcoder.h
+++ colourcoder.h
@@ -16,16 +16,5 @@ namespace TelCoColorCoder
     int numberOfMinorColors =
         sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
 
-    
-    ColorPair : GetColorFromPairNumber(int pairNumber) {
-        int zeroBasedPairNumber = pairNumber - 1;
-        MajorColor majorColor = 
-            (MajorColor)(zeroBasedPairNumber / numberOfMinorColors);
-        MinorColor minorColor =
-            (MinorColor)(zeroBasedPairNumber % numberOfMinorColors);
-        return ColorPair(majorColor, minorColor);
-    }
-    int GetPairNumberFromColor(MajorColor major, MinorColor minor) {
-        return major * numberOfMinorColors + minor + 1;
-    }    
+        
 }
