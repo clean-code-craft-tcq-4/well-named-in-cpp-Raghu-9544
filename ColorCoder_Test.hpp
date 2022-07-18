c0836a4e6f0fa13618f9b4e5a060ca94c84d382b
Raghu-9544 <100403037+Raghu-9544@users.noreply.github.com>
2022-07-18 21:20:33 +0530

    Update and rename main.h to ColorCoder_Test.hpp

---
+++ ColorCoder_Test.hpp
@@ -0,0 +1,8 @@
+// color coder Test declaration
+
+#include "ColorCoder_Function.hpp"
+
+void testNumberToPair(int pairNumber, TelCoColorCoder::MajorColor expectedMajorColor, TelCoColorCoder::MinorColor expectedMinorColor);
+void testPairToNumber( TelCoColorCoder::MajorColor pairsMajorColor, TelCoColorCoder::MinorColor pairsMinorColor, int expectedPairNumber);
+               
+
